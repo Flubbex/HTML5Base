ea13617edee8f7ebf9c6b38885cacf975bb015ae
Flubbex <michael_nijhof@live.com>
2017-06-02 16:03:44 +0200

    materialGirl update

--- source/index.js
+++ source/index.js
@@ -1,1 +1,69 @@
+$(document).ready(function(){
+            
+		  var $bodyEl = $('body'),
+			  $sidedrawerEl = $('#sidedrawer');
+        
+		  
+		  // ==========================================================================
+		  // Toggle Sidedrawer
+		  // ==========================================================================
+		  function showSidedrawer() {
+			// show overlay
+			var options = {
+			  onclose: function() {
+				$sidedrawerEl
+				  .removeClass('active')
+				  .appendTo(document.body);
+			  }
+			};
+			
+			var $overlayEl = $(mui.overlay('on', options));
+			
+			// show element
+			$sidedrawerEl.appendTo($overlayEl);
+			setTimeout(function() {
+			  $sidedrawerEl.addClass('active');
+			}, 20);
+		  }
+		  
+		  
+		  function hideSidedrawer() {
+			$bodyEl.toggleClass('hide-sidedrawer');
+		  }
+		  
+		  
+		  $('.js-show-sidedrawer').on('click', showSidedrawer);
+		  $('.js-hide-sidedrawer').on('click', hideSidedrawer);
+		  
+		  
+		  // ==========================================================================
+		  // Animate menu
+		  // ==========================================================================
+		  var $titleEls = $('strong', $sidedrawerEl);
+		  
+		  $titleEls
+			.next()
+			.hide();
+		  
+		  $titleEls.on('click', function() {
+			$(this).next().slideToggle(200);
+		  });
+          
+          $bodyEl.fadeIn(250,function(){
+            
+          $("#content").slideDown(250);
+            
+            $(".sidedrawer-toggle")
+                .delay(500)
+                .animate({opacity:1},200,"linear",function(){
+                  $(this).animate({opacity:0.5},function(){
+                      $(this).animate({opacity:1},function(){
+                            $(this).animate({opacity:0.5},200);
+                          });
+                        
+                  });
+                });
+        });
+});
+        
 module.exports = {};

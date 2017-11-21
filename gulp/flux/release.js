3e1a6b4d4d97fcabaec247d6565e0efe42fea0d9
flubbex <michael_nijhof@live.com>
2017-11-21 14:06:06 +0000

    Cleanup #2

--- gulp/flux/release.js
+++ gulp/flux/release.js
@@ -1,11 +1,13 @@
 var gulp        = require("gulp4"),
     util        = require("gulp-util"),
     rename      = require("gulp-rename");
+    insert      = require('gulp-insert');
 
 //Copy /dist/bundle.js to release/APPNAME.js
 gulp.task("release", function()
 {
     return gulp.src(config.path.dist+config.select.bundle)
     .pipe(rename(config.select.app_version))
+    .pipe(insert.append('window.cachebuster = Date.now();'))
     .pipe(gulp.dest(config.path.release))
 });
